ff048e49b60f038a209567acd179ac45d9ce9fa0
Unknown <emanuel.prado.dev@gmail.com>
2018-05-30 20:38:11 -0300

    Add host test

--- php/db_conn_var.php
+++ php/db_conn_var.php
@@ -1,5 +1,11 @@
 <?php			
 
+	$local = array('127.0.0.1', '::1');
+
+	if (in_array($_SERVER['REMOTE_ADDR'], $local)) {
+		echo 'in localhost';
+	}	
+	
 	$url = "localhost:3306";
 	$password = "";
 	$username = "root";
