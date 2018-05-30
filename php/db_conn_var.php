2b96b16340c9e39a1cd510d88bcaa5da34edd0eb
Unknown <emanuel.prado.dev@gmail.com>
2018-05-30 20:44:15 -0300

    Update DB connection

--- php/db_conn_var.php
+++ php/db_conn_var.php
@@ -1,14 +1,22 @@
 <?php			
 
 	$local = array('127.0.0.1', '::1');
+	$url = 'localhost:3306';
+	$password = '';
+	$username = '';
+	$dbname = '';			
+	$query ='';
 
 	if (in_array($_SERVER['REMOTE_ADDR'], $local)) {
-		echo 'in localhost';
-	}	
-	
-	$url = "localhost:3306";
-	$password = "";
-	$username = "root";
-	$dbname = "comp5000";
-	$query;
+		$url = "localhost:3306";
+		$password = "";
+		$username = "root";
+		$dbname = "comp5000";		
+	}else {
+		$url = "mysql427.umbler.com";
+		$password = "socialgamer";
+		$username = "wegamr";
+		$dbname = "wegamr-data";
+		$query;
+	}
 ?>
